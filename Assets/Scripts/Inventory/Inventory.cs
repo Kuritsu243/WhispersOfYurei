c69d51b92abe4a2ed5b67c3170023f960cc5db88
Pluvio <maksjl01@gmail.com>
2024-09-17 21:58:15 +0100

    removed unused functions
    Signed-off-by: Pluvio <maksjl01@gmail.com>

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -9,33 +9,6 @@ public class Inventory : MonoBehaviour
 
     private static int MAX_ITEMS = 4;
 
-    public bool AddItem(InventoryItem itemToAdd)
-    {
-        if (inventory.Count < MAX_ITEMS)
-        {
-            if (!inventory.Find(e => e.Name != itemToAdd.Name))
-            {
-                inventory.Add(itemToAdd);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public bool RemoveItem(InventoryItem itemToRemove)
-    {
-        if (inventory.Count > 0)
-        {
-            InventoryItem foundItem;
-            if (foundItem = inventory.Find(e => e.Name == itemToRemove.Name))
-            {
-                inventory.Remove(foundItem);
-                return true;
-            }
-        }
-        return false;
-    }
-
     public void DropItem(InventoryItem itemToDrop)
     {
         if (inventory.Count > 0)
